25ea5dec5e651757d4abe3134b2cc60f630f048e
luqqy <you@example.com>
2020-10-14 14:03:33 +0300

    created a new view function news_of_day that will be responsible for returning news for a specific day

--- gallery/studio/views.py
+++ gallery/studio/views.py
@@ -1,7 +1,21 @@
 from django.http  import HttpResponse
+import datetime as dt
 
 
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Gallery')
+
+def studio_of_day(request):
+    date = dt.date.today()
+    html = f'''
+       <html>
+       <body>
+       <h1>
+       {date.day}-{date.month}-{date.year}
+       </h1>
+       </body>
+       </html>
+           '''
+    return HttpResponse(html)       
