7681e9f74b4a79fa9091d387ed2a260ae696f942
luqqy <you@example.com>
2020-10-14 13:49:06 +0300

    added HttpResponse class from the django.http module. This will be responsible for returning a response to a user.

--- gallery/studio/views.py
+++ gallery/studio/views.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+from django.http  import HttpResponse
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Gallery')
