032c8118dc30aeaa70bb528957f57c2d16595964
luqqy <you@example.com>
2020-10-14 14:23:09 +0300

    registered studio app inside the settings.py

--- gallery/studio/views.py
+++ gallery/studio/views.py
@@ -1,4 +1,4 @@
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 
@@ -33,8 +33,12 @@ def convert_dates(dates):
     return day       
 
 def past_days_studio(request,past_date):
+        try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
 
     day = convert_dates(date)
     html = f'''
